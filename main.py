bf511d9873e24609db168f73ebc0d845b0808260
DuckyProgrammer5 <123731854+DuckyProgrammer5@users.noreply.github.com>
2023-04-12 16:35:39 +0300

    mmadfew4tgd majwn.pie
    i know this is stupid but hey its cool

---
+++ main.py
@@ -0,0 +1,10 @@
+# WHAT THE HELL AM I DOING HERREEE
+from tkinter import *
+
+# SET THE DUMB VARIABLES TO LIFE
+canvas_height = 400
+canvas_width = 600
+canvas_colour = "white"
+x_coord = canvas_width/2
+y_coord = canvas_height
+
